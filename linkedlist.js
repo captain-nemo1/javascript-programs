7c4a293fd3bf286d8238624c29ba0791c87adbad
Pranay Godha <godha.pranay@bluejeansnet.com>
2016-07-17 14:33:57 -0700

    Added remove function in singly linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -3,7 +3,7 @@ function LinkedList(){
 	this.size = 0;
 }
 
-LinkedList.prototype.add = function(val){
+LinkedList.prototype.append = function(val){
 	var node = {
 		data: val,
 		next: null
@@ -11,22 +11,47 @@ LinkedList.prototype.add = function(val){
 	if(this.head == null){
 		this.head = node;
 	} else {
-		var temp = this.head;
-		while(temp.next != null){
-			temp = temp.next;
+		var current = this.head;
+		while(current.next != null){
+			current = current.next;
 		}
-		temp.next = node;
+		current.next = node;
 	}
 	this.size++;
 }
 
 LinkedList.prototype.toString = function(){
 	var result = [];
-	var temp = this.head;
-	while(temp != null){
-		result.push(temp.data);
-		temp = temp.next;
+	var current = this.head;
+	while(current != null){
+		result.push(current.data);
+		current = current.next;
 	}
 
 	return result.toString();
+}
+
+LinkedList.prototype.remove = function(index){
+	if(index > 0 && index < this.size){
+		var current = this.head;
+		if(index === 0){
+			this.head = current.next;
+			this.size--;
+			return current.data;
+		}
+
+		var previous = current;
+		var current = previous.next;
+		var i = 1;
+		while(index !== i){
+			previous = current;
+			current = current.next;
+			i++;
+		}
+		previous.next = current.next;
+		this.size--;
+		return current.data;
+	} else{
+		return null;
+	}
 }
